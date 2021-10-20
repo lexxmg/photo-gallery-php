b158ab0c234638e44faa7f5a08516f80ff59d13e
mac-book <lexx.mg@gmail.com>
2021-10-20 14:47:13 +0300

    add fetch

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
 'use strict';
 
 const form = document.querySelector('.gellery-create__form'),
-      submitBtn = document.querySelector('.gellery-create__btn');
+      submitBtn = document.querySelector('.gellery-create__btn'),
+      inputWrapper = document.querySelector('.gellery-create__input-wrapper'),
+      success = document.querySelector('.gellery-create__success');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
@@ -10,13 +12,58 @@ form.addEventListener('submit', event => {
   submitBtn.disabled = true;
   //console.log(formData.get('file[]'));
 
-  fetch('/php/get-files.php', {
+  sendData('/php/get-files.php', formData).then(res => {
+    form.reset();
+    submitBtn.disabled = false;
+
+    if (res.error.length > 0) {
+      success.classList.add('hide');
+      removeAllElements( document.querySelectorAll('.gellery-create__error') );
+
+      res.error.forEach(item => {
+        inputWrapper.insertAdjacentHTML('beforeEnd', `
+          <span class="gellery-create__error">${item}</span>
+        `);
+      });
+
+      setTimeout(() => {
+        removeAllElements( document.querySelectorAll('.gellery-create__error') );
+      }, 5000);
+    }
+
+    if (res.success) {
+      removeAllElements( document.querySelectorAll('.gellery-create__error') );
+
+      success.classList.remove('hide');
+      success.textContent = res.success;
+
+      setTimeout(() => {
+        success.classList.add('hide');
+      }, 3000);
+    }
+    console.log(res);
+  });
+});
+
+
+function sendData(url, formData) {
+  return fetch(url, {
     method: 'POST',
     body: formData
-  }).then( res => res.json() )
-    .then(data => {
-      console.log(data);
-      form.reset();
-      submitBtn.disabled = false;
-    });
-});
+  }).then(res => {
+    if (!res.ok) {
+      throw new Error(`Ошибка ${url} ${res}`);
+    } else {
+      return res;
+    }
+  }).then(res => res.json() )
+    .then(data => data);
+}
+
+function removeAllElements(elements) {
+  elements.forEach(item => {
+    item.remove();
+  });
+
+  return true;
+}
--- templates/gallery-create.php
+++ templates/gallery-create.php
@@ -1,72 +1,4 @@
 
-<?php
-const MAX_SIZE = 2 * 1024 * 1024;
-const MAX_COUNT = 6;
-const VALID_FORMAT = ['image/jpeg', 'image/png', 'image/jpg'];
-
-$success = '';
-$error = [];
-
-// var_dump( !empty($error) );
-// var_dump( isset($error) );
-
-if ( isset($_POST['upload']) ) {
-    //echo 'была нажата кнопка';
-    //var_dump($_FILES['file']);
-    if ( !empty($_FILES['file']['name']) ) {
-        if ( $_FILES['file']['name'][0] === '' ) {
-            $error[] = 'Нужно выбрать хотя бы один файл!';
-        } else if ( count($_FILES['file']['name']) > MAX_COUNT ) {
-            $error[] = 'Можно загрузить максимум ' . MAX_COUNT . ' файлов!';
-        } else {
-            foreach ($_FILES['file']['name'] as $i => $item) {
-                $finfo = finfo_open(FILEINFO_MIME_TYPE);
-                $type =  finfo_file($finfo, $_FILES['file']['tmp_name'][$i]);
-                finfo_close($finfo);
-
-                if ($_FILES['file']['size'][$i] > MAX_SIZE) {
-                    $success = '';
-                    $error[] = 'Размер файла не може привышать 2Mb ' . $item;
-                    //break;
-                }
-
-                foreach (VALID_FORMAT as $j => $value) {
-                    $err = true;
-
-                    if ($type === $value) {
-                        $err = false;
-                        break;
-                    }
-                }
-
-                if ($err) {
-                    $error[] = 'Не допустимый формат файла! ' . $item;
-                }
-            }
-        }
-
-        if ( empty($error) ) {
-            foreach ($_FILES['file']['error'] as $key => $value) {
-                if ( $value === UPLOAD_ERR_OK ) {
-                    $tmp_name = $_FILES['file']['tmp_name'][$key];
-                    // basename() может предотвратить атаку на файловую систему;
-                    // может быть целесообразным дополнительно проверить имя файла
-                    $name = basename($_FILES['file']['name'][$key]);
-                    //$name = $_FILES['file']['name'][$key];
-                    $name = preg_replace('/[^\w-]/', '_', $name);
-                    move_uploaded_file($tmp_name, "$pathToUpload/$name" );
-                    $success = 'Файлы успешно загруженны';
-                } else {
-                    $error[] = "Произошла ошибка $value, повторите попытку позже.";
-                    break;
-                }
-            }
-        }
-    }
-}
-
-?>
-
 <h1 class="gellery-create-tiile">Загрузить фото</h1>
 
 <form class="gellery-create__form" name="myForm" action="" method="post" enctype="multipart/form-data">
@@ -80,15 +12,7 @@ if ( isset($_POST['upload']) ) {
     >
     <label class="gellery-create__label" for="file">Выберете файл</label>
 
-    <?php if ( !empty($error) ): ?>
-        <?php foreach ($error as $key => $value): ?>
-            <span class="gellery-create__error"><?=$value?></span>
-        <?php endforeach; ?>
-    <?php endif; ?>
-
-    <?php if ($success): ?>
-        <span class="gellery-create__success"><?=$success?></span>
-    <?php endif; ?>
+    <span class="gellery-create__success hide">success</span>
   </div>
 
   <button class="gellery-create__btn" name="upload">Загрузить</button>
