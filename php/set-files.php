4b34e889ea0d1d493ddcc1ff3cbfbd92d6e1cdbd
mac-book <lexx.mg@gmail.com>
2021-10-28 14:26:11 +0300

    sync

--- php/set-files.php
+++ php/set-files.php
@@ -54,6 +54,12 @@ if ( isset($_POST['upload']) ) {
                     break;
                 }
             }
+        } else {
+            ini_set('display_errors', 1);
+
+            ini_set('display_startup_errors', 1);
+
+            error_reporting(E_ALL);
         }
     }
 }
